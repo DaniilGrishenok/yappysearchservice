364e7e11f4145e3622723580b17a410c1e6691df
Daniil Grishenok <grishenokdaniil5@gmail.com>
2024-06-04 14:05:38 +0300

    initial commit

---
+++ src/main/java/ru/shsh/yappysearchservice/YappysearchserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.shsh.yappysearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YappysearchserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YappysearchserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shsh/yappysearchservice/YappysearchserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shsh.yappysearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YappysearchserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
